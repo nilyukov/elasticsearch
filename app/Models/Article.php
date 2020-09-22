14751ac79d9c25e4733cf7c3257ef92bab91b8cc
nilyukov <nilyukov@gmail.com>
2020-09-22 21:56:04 +0300

    Import Searchable trait to Article Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $casts = [
         'tags' => 'json',
