20ed76ebf2eee11d669c89e0a490a6d268caf51a
nilyukov <nilyukov@gmail.com>
2020-09-22 21:57:04 +0300

    Create Elasticesearch Repository with Articles Repository implement

---
+++ app/Repositories/ElasticSearch/ElasticsearchRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Repositories\ElasticSearch;
+
+
+use App\Models\Article;
+use App\Repositories\Articles\ArticlesRepository;
+use Elasticsearch\Client;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
+
+class ElasticsearchRepository implements ArticlesRepository
+{
+    private $elasticsearch;
+
+    public function __construct(Client $elasticsearch)
+    {
+        $this->elasticsearch = $elasticsearch;
+    }
+
+    public function search(string $query = ''): Collection
+    {
+        $items = $this->searchOnElasticsearch($query);
+        return $this->buildCollection($items);
+    }
+
+    private function searchOnElasticsearch(string $query = ''): array
+    {
+        $model = new Article;
+        $items = $this->elasticsearch->search([
+            'index' => $model->getSearchIndex(),
+            'type' => $model->getSearchType(),
+            'body' => [
+                'query' => [
+                    'multi_match' => [
+                        'fields' => ['title^5', 'body', 'tags'],
+                        'query' => $query,
+                    ],
+                ],
+            ],
+        ]);
+        return $items;
+    }
+
+    private function buildCollection(array $items): Collection
+    {
+        $ids = Arr::pluck($items['hits']['hits'], '_id');
+        return Article::findMany($ids)
+            ->sortBy(function ($article) use ($ids) {
+                return array_search($article->getKey(), $ids);
+            });
+    }
+}
