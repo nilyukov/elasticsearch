11bc47521f1d71080495f0774614883d7709b746
nilyukov <nilyukov@gmail.com>
2020-09-22 22:17:35 +0300

    Register Elasticsearch and Articles Repositories into Service Provider; Bind Elasticsearch Client;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 
 use App\Repositories\Articles\ArticlesRepository;
 use App\Repositories\Articles\EloquentRepository;
+use App\Repositories\ElasticSearch\ElasticsearchRepository;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,10 +19,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            ArticlesRepository::class,
-            EloquentRepository::class
-        );
+        $this->app->bind(ArticlesRepository::class, function(){
+            // Это полезно, если мы хотим выключить наш кластер
+            // или при развертывании поиска на продакшене
+            if (! config('services.search.enabled')) {
+                return new EloquentRepository();
+            }
+            return new ElasticsearchRepository(app(Client::class));
+        });
+
+        $this->bindSearchClient();
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 
     /**
